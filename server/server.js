cd087dc3e49a055ceb0674313adb229dcc98462d
Joe Flanagan <josephptflanagan@gmail.com>
2020-10-14 09:37:00 -0500

    updated back end

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import AuthMiddleware
+const { authMiddleware } = require('./utils/auth');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+// create a new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// integrate  Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,22 +5,35 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+
+  signToken: function ({ username, email, _id }) {
+
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+
+  },
+
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
+    // if no token, return request object as is
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
-    // verify token and get user data out of it
+   
     try {
+      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
@@ -28,12 +41,8 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return updated request object
+    return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
 };
